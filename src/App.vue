8bc7351112144d581d11bded1ea39e8b9a10ce74
Shane <shanec98@outlook.com>
2024-01-29 11:46:07 +0100

    Moved home screen content into Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
-  <!-- content above buttons -->
-  <div class="relative h-svh w-full">
-    <div class="absolute inset-x-0 bottom-0 flex h-1/2">
-      <section-button class="basis-1/3" section="about" name="About"></section-button>
-      <section-button class="basis-1/3" section="work" name="Work"> </section-button>
-      <section-button class="basis-1/3" section="contact" name="Contact"> </section-button>
-    </div>
-  </div>
-
   <!-- components associated with these routes will be rendered inside the <router-view> when their routes are activated-->
   <router-view></router-view>
 </template>
 
 <script>
-import SectionButton from './components/SectionButton.vue'
 export default {
-  name: 'App',
-  components: {
-    SectionButton
-  }
+  name: 'App'
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,23 @@
 <template>
+  <!-- content above buttons -->
+
+  <div class="relative h-svh w-full">
+    <div class="absolute inset-x-0 bottom-0 flex h-1/2">
+      <section-button class="basis-1/3" section="about" name="About"></section-button>
+      <section-button class="basis-1/3" section="work" name="Work"> </section-button>
+      <section-button class="basis-1/3" section="contact" name="Contact"> </section-button>
+    </div>
+  </div>
   <p>Hello Home</p>
 </template>
+
 <script>
+import SectionButton from '@/components/SectionButton.vue'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    SectionButton
+  }
 }
 </script>
