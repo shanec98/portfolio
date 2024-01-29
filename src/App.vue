da00e739e1f8698138d00af6e252d0da32b83177
Shane <shanec98@outlook.com>
2024-01-29 11:42:30 +0100

    buttons on home layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
-  <div class="h-screen flex flex-col">
-    <div class="flex-grow bg-gray-200">
-      <div class="container h-3/5">
-        <!-- content above buttons -->
-
-        <div>
-          <section-button section="about" name="About"></section-button>
-          <section-button section="work" name="Work"> </section-button>
-          <section-button section="contact" name="Contact"> </section-button>
-        </div>
-
-        <!-- components associated with these routes will be rendered inside the <router-view> when their routes are activated-->
-        <router-view></router-view>
-      </div>
+  <!-- content above buttons -->
+  <div class="relative h-svh w-full">
+    <div class="absolute inset-x-0 bottom-0 flex h-1/2">
+      <section-button class="basis-1/3" section="about" name="About"></section-button>
+      <section-button class="basis-1/3" section="work" name="Work"> </section-button>
+      <section-button class="basis-1/3" section="contact" name="Contact"> </section-button>
     </div>
   </div>
+
+  <!-- components associated with these routes will be rendered inside the <router-view> when their routes are activated-->
+  <router-view></router-view>
 </template>
 
 <script>
